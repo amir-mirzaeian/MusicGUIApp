70cff4cdbc873095a8ef7acf2901bb433f0ff52a
Amir Mirzaeian <amir.mirzaeian2012@gmail.com>
2019-04-14 20:37:18 +0200

    Initial Commit

--- src/com/amir/music/Main.java
+++ src/com/amir/music/Main.java
@@ -1,6 +1,8 @@
 package com.amir.music;
 
+import com.amir.music.model.Datasource;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,13 +13,28 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Music Application");
+        primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        if (!Datasource.getInstance().open()){
+            System.out.println("Problem with opening the database.");
+            Platform.exit();
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        Datasource.getInstance().close();
     }
 }
